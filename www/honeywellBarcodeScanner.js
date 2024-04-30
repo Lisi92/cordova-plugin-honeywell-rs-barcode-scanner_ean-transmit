1c793562a7ec29924d0126f3bf35df35cf966031
Christina Liesenfeld <christina.liesenfeld@retailsolutions.de>
2024-04-30 14:49:21 +0200

    add notification

--- www/honeywellBarcodeScanner.js
+++ www/honeywellBarcodeScanner.js
@@ -3,5 +3,9 @@ var exec = require('cordova/exec');
 module.exports = {
     onBarcodeScanned: function(success, failure){
         return exec(success, failure, "HoneywellBarcodeScannerPlugin", "onBarcodeScanned", []);
-    }
+    },
+	
+	errorNotification: function(success, failure){
+		return exec(success, failure, "HoneywellBarcodeScannerPlugin", "onFailureEvent", []);
+	}
 };
